b936255edc6c8e9a80516c61604800107837c99f
tim-morozov <tim.morozov@gmail.com>
2020-07-31 13:17:42 -0500

    Fixed mongoURI error

--- index.js
+++ index.js
@@ -1,13 +1,16 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const bodyparser = require('body-parser');
 
 const app = express();
+
+app.use(bodyparser.json());
 
 const db = require('./config/keys').mongoURI;
 
 mongoose.connect(db)
-.then(() => console.log("MongoDb connected..."))
-.catch(err => console.log(err));
+    .then(() => console.log('MongoDB Connected...'))
+    .catch(err => console.log(err));
 
 
 const port = process.env.PORT || 5000;
