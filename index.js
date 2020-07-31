1baee80e3055e250770610390d2aad11ae4e03ee
tim-morozov <tim.morozov@gmail.com>
2020-07-30 22:02:35 -0500

    connected to mongodb

--- index.js
+++ index.js
@@ -1,3 +1,15 @@
 const express = require('express');
+const mongoose = require('mongoose');
 
 const app = express();
+
+const db = require('./config/keys').mongoURI;
+
+mongoose.connect(db)
+.then(() => console.log("MongoDb connected..."))
+.catch(err => console.log(err));
+
+
+const port = process.env.PORT || 5000;
+
+app.listen(port, () => console.log(`Server started on ${port}`));
